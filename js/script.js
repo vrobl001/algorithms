8d94806fb9899482f3abdf02a2bf19328793f362
Vincent Robles <sylkisgsx@gmail.com>
2020-08-06 19:41:08 -0400

    added compressing boxes example

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const { performance } = require('perf_hooks');
 
+// Finding Nemo!
 // O(n) --> Linear Time
+// Complexity = Fair
+
 const nemo = ['Nemo'];
 const indianOcean = ['Dory', 'Bruce', 'Marlin', 'Gill', 'Bloat', 'Nigel', 'Squirt', 'Darla', 'Nemo'];
 const large = new Array(20).fill('Nemo');
@@ -25,3 +28,18 @@ findNemo(nemo);
 console.log(' ');
 console.log('Large: ');
 findNemo(large);
+
+// Compress Boxes
+// O(n) -- Linear Time
+// Complexity = Fair
+const boxes = new Array(30).fill('box');
+const compressAllBoxes = (boxes) => {
+  let t0 = performance.now();
+  boxes.forEach((box) => console.log('Box Compressed!'));
+  let t1 = performance.now();
+  console.log('It took ' + (t1 - t0) + 'milliseconds to compress all boxes!');
+};
+
+console.log(' ');
+console.log('Machine is compressing boxes!');
+compressAllBoxes(boxes);
